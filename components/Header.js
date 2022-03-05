d73a21a1c563d6b22a329dc06f4093612c541386
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-03-05 23:50:05 +0000

    header component complete

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,32 @@
 import React from "react";
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function Header() {
 	return (
-        <header>
-            <div>
-                <Image
-                    src='https://links.papareact.com/qd3'
-                    alt=''
-                    layout="fill"
-                    width={40}
-                    height={40}
-                />
-            </div>
+		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+			<div className="relative flex items-center h-10 cursor-pointer my-auto">
+				<Image
+					src="https://links.papareact.com/qd3"
+					alt=""
+					layout="fill"
+					objectFit="contain"
+					objectPosition="left"
+				/>
+			</div>
 
-            <div></div>
+			<div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+				<input
+					className="flex-grow pl-5 bg-transparent outline-none placeholder-gray-400 text-gray-600"
+					type="text"
+					placeholder="Start your search"
+				/>
+				<SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+			</div>
 
-            <div></div>
-			<h1>This is the Header</h1>
+			<div>
+				<p>Become a host</p>
+			</div>
 		</header>
 	);
 }
